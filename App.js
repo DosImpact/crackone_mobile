d7b07ae8408aaac32c1677a667de87d5d7141986
DosImpact <ypd03008@gmail.com>
2020-12-30 12:47:37 +0900

    update issue

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import * as React from "react";
+import { BackHandler } from "react-native";
 import { WebView } from "react-native-webview";
 import { StatusBar, SafeAreaView } from "react-native";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.WEBVIEW_REF = React.createRef();
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  componentWillUnmount() {
+    BackHandler.removeEventListener("hardwareBackPress", this.handleBackButton);
+  }
+
+  handleBackButton = () => {
+    this.WEBVIEW_REF.current.goBack();
+    return true;
+  };
+
+  onNavigationStateChange(navState) {
+    this.setState({
+      canGoBack: navState.canGoBack,
+    });
+  }
+
   render() {
     return (
       <>
@@ -13,6 +38,9 @@ export default class App extends React.Component {
             // onNavigationStateChange={(e) => console.log(e)}
             // onMessage={(e) => console.log(e)}
             style={{ marginTop: 0 }}
+            mixedContentMode="always"
+            ref={this.WEBVIEW_REF}
+            onNavigationStateChange={this.onNavigationStateChange.bind(this)}
           />
         </SafeAreaView>
       </>
